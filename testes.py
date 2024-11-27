ae9835e7bd8fb22f19fccef006a259d2f8432531
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2024-11-27 11:39:23 -0300

    v61

--- testes.py
+++ testes.py
@@ -62,10 +62,10 @@ class SessaoJurisprudencia:
         """Coletando os documentos usando o TokenCaptcha"""
         try:
             if not self.token_desafio or not self.resposta_captcha:
-                print("Token Desafion ou resposta ausentes")
+                print("Token Desafio ou resposta ausentes")
                 return
             token_captcha = self.token_desafio
-            url_documentos = f"{URL_DOCUMENTOS}?tokenCaptcha={self.token_captcha}"
+            url_documentos = f"{URL_DOCUMENTOS}?tokenCaptcha={token_captcha}"
             print(f"Requisitando documentos na URL: {url_documentos}")
 
             resposta = self.sessao.get(url_documentos)
@@ -98,13 +98,16 @@ class SessaoJurisprudencia:
     
 
     def resolver_captcha(self, base64_string):
-        """Resolvendo o captcha"""
+        """Resolvendo o Captcha"""
         try:
             base64_string = base64_string.split(',')[1] if base64_string.startswith('data:image') else base64_string
-            self.resposta_captcha = solve_captcha_local(base64_string)
+            resposta = solve_captcha_local(base64_string)
+            self.resposta_captcha = resposta
             print(f"Resposta do Captcha: {self.resposta_captcha}")
+            return resposta
         except Exception as e:
             print(f"Erro ao resolver o Captcha: {e}")
+            self.resposta_captcha = None
 
     
     def enviar_resposta_captcha(self):
@@ -132,47 +135,55 @@ class SessaoJurisprudencia:
             print(f"Erro ao enviar a resposta captcha: {e}")
 
 
-    def enviar_documentos(self):
-        """Realizando o POST para enviar o documento com o token a resposta do Captcha"""
+    def enviar_documento(self):
+        """Realiza o POST para enviar o documento com o token e a resposta do captcha."""
         try:
             if not self.token_desafio or not self.resposta_captcha:
-                print("Token de desafio ou resposta do captcha ausentes")
+                print("Token de desafio ou resposta do captcha ausente.")
                 return
-
             url_post = (
                 f"{URL_DOCUMENTOS}?tokenDesafio={self.token_desafio}&resposta={self.resposta_captcha}"
             )
             print(f"URL montada para o POST: {url_post}")
-
-            payload = {}  
-            headers = {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'User-Agent': 'Mozilla/5.0'
+            payload = {
+                "resposta": self.resposta_captcha, 
+                "tokenDesafio": self.token_desafio 
             }
-
+            headers = {
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+                'Connection': 'keep-alive',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+                'Referer': URL_BASE,
+                'Origin': 'https://pje.trt2.jus.br',
+                'Authorization': f"Bearer {self.token_desafio}",
+                'Cache-Control': 'no-cache',
+            }
             resposta = self.sessao.post(url_post, json=payload, headers=headers)
             print(f"Resposta do servidor: {resposta.status_code} - {resposta.text}")
 
             if resposta.status_code == 200:
-                print("POST realizado com sucesso")
+                print("POST realizado com sucesso.")
                 return resposta.json()
             else:
-                print(f"Erro ao realizar o POST: {resposta.status_code} - {resposta.text}")
+                print(f"Erro ao realizar POST: {resposta.status_code} - {resposta.text}")
         except Exception as e:
-            print(f"Erro ao realizar o envio do POST: {e}") 
+            print(f"Erro ao enviar o documento: {e}")
 
-    
+
+            
+                
     def inciando_sessao(self):
         """Fluxo de iniciar a Sessão"""
         try:
             print("Iniciando a Sessão")
 
-            cookies = self.obter_cookie(URL_BASE)
+            '''cookies = self.obter_cookie(URL_BASE)
             if cookies:
                 print(f"Cookies coletados: {cookies}")
             else:
-                print("Erro ao coletar o Cookie")
+                print("Erro ao coletar o Cookie")'''
             
             self.fazer_requisicao_hearders(URL_CAPTCHA)
 
@@ -184,7 +195,7 @@ class SessaoJurisprudencia:
             
             self.enviar_resposta_captcha()
 
-            self.enviar_documentos()
+            self.enviar_documento()
 
 
 
