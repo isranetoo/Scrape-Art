423fe8dd04c4a82fc45814bc812065a32e418b22
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2024-12-06 12:44:23 -0300

    v 1.0.0

--- coleta_processos.py
+++ coleta_processos.py
@@ -1,6 +1,7 @@
 import json
 
-arquivo_json = "processos.json"
+arquivo_entrada = "processos.json"
+arquivo_saida = "informacoes_processos.json"
 
 def coletar_informacoes(arquivo, campos_para_coletar):
     """
@@ -20,9 +21,7 @@ def coletar_informacoes(arquivo, campos_para_coletar):
                 documentos = dados["documents"]
                 infomacoes = []
                 for doc in documentos:
-                    entrada = {}
-                    for campo in campos_para_coletar:
-                        entrada[campo] = doc.get(campo, "Não disponível")
+                    entrada = {campo: doc.get(campo, "Não disponível") for campo in campos_para_coletar}
                     infomacoes.append(entrada)
             else:
                 raise ValueError("Formato JSON não encotrado ou chave 'documents' ausente")
@@ -38,14 +37,26 @@ def coletar_informacoes(arquivo, campos_para_coletar):
         print(f"Erro inesperado: {e}")
         return []
     
+def salvar_informacoes_em_arquivo(informacoes, arquivo):
+        """
+            Salvar informações coletadas em um arquivo JSON.
+
+            Args:
+                informacoes (list): Lista de informações coletadas.
+                arquivo (str): Nome do arquivo de sáida.
+        """
+        try:
+            with open(arquivo, 'w', encoding='utf-8') as f:
+                json.dump({"documents": informacoes}, f, ensure_ascii=False, indent=4)
+            print(f"Informações salvas com \033[1;32mSUCESSO\033[0m no arquivo \033[1;34m'{arquivo}'\033[0m")
+        except Exception as e:
+            print(f"Erro ao salvar as informações: {e}")
+    
 if __name__ == "__main__":
-    campos = ["processo", "anoProcesso", "classeJudicial", "dataPublicacao"]
-    informacoes = coletar_informacoes(arquivo_json, campos)
+    campos = ["sigiloso","anoProcesso","tipoDocumento","dataDistribuicao", "instancia", "processo", "orgaoJulgador", "magistrado", "classeJudicialSigla", "classeJudicial", "dataPublicacao",]
+    informacoes = coletar_informacoes(arquivo_entrada, campos)
     if informacoes:
-        print("Informações coletadas:")
-        for idx, info in enumerate(informacoes, start=1):
-            detalhes = " - ".join([f"{campo}: {valor}" for campo, valor in info.items()])
-            print(f"{idx}. {detalhes}")
+        salvar_informacoes_em_arquivo(informacoes, arquivo_saida)
     else:
-        print("Nenhum informação encotrada.")
+        print("Nenhum informação encotrada para salvar.")
 
